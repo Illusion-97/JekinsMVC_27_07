fcca370a903c1ade1394501420ba4d3f6e39e53b
Yanis Adekalom <yanis971.60@gmail.com>
2023-07-27 10:32:54 +0200

    Initial commit

---
+++ src/test/java/fr/dawan/jekinsmvc/JekinsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.jekinsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
